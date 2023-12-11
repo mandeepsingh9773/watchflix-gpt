2320d190fa268652bff39e6e107996da887d2a2c
Mandeep Singh Kachawa <mandeepsingh9773@gmail.com>
2023-12-11 19:12:54 +0530

    Created Sign/Signup form

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React from 'react';
+import Login from "./Login";
+import Browse from "./Browse";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 const Body = () => {
+      
+      const appRouter =createBrowserRouter([
+        {
+          path: "/",
+          element: <Login />,
+        },
+        {
+          path:"/browse",
+          element: <Browse />,
+        }
+      ]);
+
+
   return (
-    <div>Body</div>
-  )
+    <div>
+    <RouterProvider router={appRouter} />
+    </div>
+  );
 }
 
-export default Body
+export default Body;

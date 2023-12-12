aac523fe5a1e4b70188b11225feeec4887789f25
Mandeep Singh Kachawa <mandeepsingh9773@gmail.com>
2023-12-13 01:09:32 +0530

    Implemented redux and updated SignIN&OUT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import Body from "./Components/Body"
+import "./App.css";
+import { Provider } from "react-redux";
+import Body from "./Components/Body";
+import appStore from "./Utils/appStore";
+
 function App() {
   return (
-    <div>
-       <Body />
-    </div>
+    <Provider store={appStore}>
+      <Body />;
+    </Provider>
   );
 }
 
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -4,17 +4,25 @@ import { checkValidData } from "../Utils/validate.js";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../Utils/Firebase.js";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addUser } from "../Utils/userSlice.js";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMeassage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
 
+  const name = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
 
@@ -34,14 +42,31 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/92371379?v=4",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName } = auth.currentUser;
+              dispatch(
+                addUser({ uid: uid, email: email, displayName: displayName })
+              );
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMeassage(error.message);
+            });
           console.log(user);
-          // ...
+          // navigate("/browse");
+
+         
         })
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
           setErrorMeassage(errorCode + "-" + errorMessage);
-          // ..
+         
         });
     } else {
       //Sign In Logic
@@ -54,6 +79,7 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse");
 
           // ...
         })
@@ -82,6 +108,7 @@ const Login = () => {
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-2 w-full bg-gray-800 rounded-lg"
